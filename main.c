779946770d1a03ed6c119abc532e4b2e01fbbdd8
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2025-09-02 01:40:19 +0300

    Delete main.c

--- main.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-
-int main(void)
-{
-	printf("%c %s %p %d %i %u %x %X ", 'x', "mohammad alhindi", NULL, 56, 56, 198, 189);	
-	fr_prinf("%c %s %p %d %i %u %x %X ", 'x', "mohammad alhindi", NULL, 56, 56, 198, 189);
-}
